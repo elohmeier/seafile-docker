1a7dbd085bb34a5f856f6ef0828db40918674d88
Enno Lohmeier <enno.lohmeier@nerdworks.de>
2017-04-19 11:55:06 +0200

    switch to mysql and use newer image

--- scripts/bootstrap.py
+++ scripts/bootstrap.py
@@ -129,7 +129,7 @@ def init_seafile_server():
         'MYSQL_USER': 'seafile',
         'MYSQL_USER_PASSWD': str(uuid.uuid4()),
         'MYSQL_USER_HOST': '127.0.0.1',
-        # Default MariaDB root user has empty password and can only connect from localhost.
+        # Default MySQL root user has empty password and can only connect from localhost.
         'MYSQL_ROOT_PASSWD': '',
     }
 
